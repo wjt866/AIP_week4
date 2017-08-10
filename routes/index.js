97c86b88962fe0d0efa9948ad610ad68f5023a70
William <williamthomson866@gmail.com>
2017-08-10 11:10:43 +1000

    adding routes and json data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.sendFile(path.join(__dirname, '../', 'views', 'index.html'));
+});
+
+router.get('/data', function(req, res, next) {
+    res.json([{"title": "People Places Things"},
+        {"releaseDate": "14/08/2015"},
+        {"duration": "85 minutes"},
+        {"genre": "Comedy"},
+        {"synopsis": "Will Henry is a newly single graphic novelist..."},]);
 });
 
 module.exports = router;
